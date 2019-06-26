de62963f1d4fa1541d5b4a0cd693a85c16ce5606
David Rojas <david_rojas82142@elpoli.edu.co>
2019-06-26 16:45:27 -0500

    returns undefined on no args

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,8 +1,10 @@
 import isArray from './utils';
 
 export function min(...params) {
+  if (params.length === 0) return undefined;
+
   return isArray(...params)
-    ? Math.min(...params.reduce((acc, val) => acc.concat(val), [])) 
+    ? Math.min(...params.reduce((acc, val) => acc.concat(val), []))
     : Math.min(...params);
 }
 
