0b067007cd4121b1fab328b7487f44c9809f82cc
David Rojas <david_rojas82142@elpoli.edu.co>
2019-06-27 10:22:16 -0500

    delay will not wait if the time is too long

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,17 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(TIME) {
+  const startTime = Date.now();
+  return new Promise((resolve, reject) => {
+    if (TIME > 4999) reject(Error('This time is too much !'));
+    setInterval(() => {
+      const endTime = Date.now();
+      const delayTime = endTime - startTime;
+      if (delayTime < delayTime + 100 || delayTime > delayTime - 100) {
+        resolve(delayTime);
+      }
+    }, TIME);
+  });
 }
 
 export function asyncDelay() {
