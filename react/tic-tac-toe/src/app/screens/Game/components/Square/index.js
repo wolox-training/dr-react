ff4d6903088ade01988c11a73993fe9bff151bde
David Rojas <david_rojas82142@elpoli.edu.co>
2019-06-28 10:46:39 -0500

    Passing data through props

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
-const Square = () => (
+const Square = ({ value }) => (
   <button type="button" className={styles.square}>
-    {/* TODO */}
+    {value}
   </button>
 );
+
+Square.propTypes = {
+  value: PropTypes.string.isRequired
+};
 
 export default Square;
--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './styles.module.scss';
 import Board from './components/Board';
 
-function Game () {
+function Game() {
   return (
     <div className={styles.game}>
       <div className={styles.gameBoard}>
